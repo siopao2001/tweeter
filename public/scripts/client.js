6cc6249bc84c9a4a7451a997de7e590d390e8232
siopao2001 <sio_pao2001@yahoo.ca>
2021-09-25 16:59:16 +0000

    Remove commented out code and linted client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,51 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
 
-//   const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd" },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     },
-//     {
-//       "user": {
-//         "name": "Paolo",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@me_ing" },
-//       "content": {
-//         "text": "Test!"
-//       },
-//       "created_at": 1573245023718
-//     }
-//   ]
-const escaper =  function(str) {
+
+$(document).ready(function() {
+  const escaper = function(str) {
     let tweet = document.createElement('div');
     tweet.appendChild(document.createTextNode(str));
     return tweet.innerHTML;
   }
-
   const createTweetElement = function(tweetObj) {
     const $tweet = $("<article>").addClass("tweet");
-
     const innerHTML = `
           <header>
               <img src= ${tweetObj.user.avatars}>
@@ -61,30 +26,25 @@ const escaper =  function(str) {
             <h4><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></h4>
           </footer>
           `;
-
     $tweet.append(innerHTML);
     return $tweet;
   };
-
   const renderTweets = function(tweetObjArr) {
     for (const tweet of tweetObjArr) {
       const $tweet = createTweetElement(tweet);
       $('section.all-tweets').prepend($tweet);
     }
   };
-
-  //renderTweets(data);
-
-const loadTweets = function () {
-    $.ajax('/tweets', { method: 'GET' })
-    .then(function (allTweets) {
-      renderTweets(allTweets);
-    })
+  const loadTweets = function() {
+    $.ajax('/tweets', {
+        method: 'GET'
+      })
+      .then(function(allTweets) {
+        renderTweets(allTweets);
+      })
   }
-
-loadTweets()
-
-$('.new-tweet form').submit( function (event) {
+  loadTweets()
+  $('.new-tweet form').submit(function(event) {
     event.preventDefault();
     const $form = $(this);
     $('.errorMessage').empty().slideUp();
@@ -98,16 +58,20 @@ $('.new-tweet form').submit( function (event) {
     } else {
       $('.errorMessage').slideUp();
       const tweet = $form.serialize();
-      $.ajax({ url: "/tweets/", method: 'POST', data: tweet }) 
-      .then (function (postRequestReturnValue) {
-        return $.ajax('/tweets', { method: 'GET' })
-      })
-      .then (function (getRequestReturnValue) {
-        const latestTweet = [getRequestReturnValue[getRequestReturnValue.length - 1]];
-        renderTweets(latestTweet);
-      })
+      $.ajax({
+          url: "/tweets/",
+          method: 'POST',
+          data: tweet
+        })
+        .then(function(postRequestReturnValue) {
+          return $.ajax('/tweets', {
+            method: 'GET'
+          })
+        })
+        .then(function(getRequestReturnValue) {
+          const latestTweet = [getRequestReturnValue[getRequestReturnValue.length - 1]];
+          renderTweets(latestTweet);
+        })
     }
   })
-
-
 });
